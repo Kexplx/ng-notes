8b3924a50124b6043c83ebcbcc3b5d914bfffd50
kexplx <oscar.rosner@web.de>
2020-03-23 20:27:16 +0100

    adds note-list component

---
+++ src/app/notes/note-list/note-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { Note } from 'src/app/models/note.model';
+import { NoteService } from '../note.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-note-list',
+  templateUrl: './note-list.component.html',
+  styleUrls: ['./note-list.component.scss'],
+
+  animations: [
+    trigger('zoomIn', [
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(200)
+      ])
+    ])
+  ]
+})
+export class NoteListComponent implements OnInit {
+  notes: Note[];
+  constructor(private noteService: NoteService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.notes = this.noteService.getNotes();
+  }
+
+  handleAction(action: string) {
+    switch (action) {
+      case 'add':
+        const noteId = this.noteService.addNote();
+        const urlTree = this.router.createUrlTree(['notes', noteId]);
+        this.router.navigateByUrl(urlTree);
+        break;
+
+      default:
+        console.log('Invalid action.');
+        break;
+    }
+  }
+}
