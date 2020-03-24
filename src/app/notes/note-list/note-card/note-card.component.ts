e994a31cfe5ee4d96f27fea463491bad043d81b2
kexplx <oscar.rosner@web.de>
2020-03-24 03:19:03 +0100

    adds onDelete method to emit deleteClicked

--- src/app/notes/note-list/note-card/note-card.component.ts
+++ src/app/notes/note-list/note-card/note-card.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Note } from 'src/app/models/note.model';
-import { NoteColor } from 'src/app/models/note-types.enum';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
 export class NoteCardComponent implements OnInit {
-
+  @Output() deleteClicked = new EventEmitter();
   @Input() note: Note;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete() {
+    this.deleteClicked.emit(this.note.id);
   }
 }
