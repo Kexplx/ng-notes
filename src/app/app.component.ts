595f8d2c6b61d46a8f1a5aa8890300870df0907a
kexplx <oscar.rosner@web.de>
2020-03-24 01:10:43 +0100

    adds todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+/**
+ * Delete on each card in card-list
+ * Error routes handling
+ *      note not found...
+ *      general gibberish
+ * Settings, light-dark mode
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
