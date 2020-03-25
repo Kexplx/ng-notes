cafc79efc5043ae174e538261b1a4d6a16a8a79d
kexplx <oscar.rosner@web.de>
2020-03-25 02:33:11 +0100

    removes title property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
-/**
- * Delete on each card in card-list
- * Error routes handling
- *      note not found...
- *      general gibberish
- * Settings, light-dark mode
- */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'notes';
+
 }
